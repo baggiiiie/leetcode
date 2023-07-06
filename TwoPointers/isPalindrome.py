1d8283b23ba085931831789b653714767de1cc8b
baggiiiie <dave23eeeeem@gmail.com>
2023-07-06 14:57:26 +0800

    Update

--- TwoPointers/isPalindrome.py
+++ TwoPointers/isPalindrome.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def isPalindrome(self, s):
+    def isPalindrome1(self, s):
         """
         :type s: str
         :rtype: bool
@@ -10,8 +10,6 @@ class Solution(object):
 
         # deal with special characters:
         # 1. copy the string without special char
-        # 2. jump ahead when it's special char
-        # deal with two special characters back to back
 
         s = s.lower()
         c = ""
@@ -28,7 +26,34 @@ class Solution(object):
             r -= 1
         return True
 
+    def isPalindrome2(self, s):
+            """
+            :type s: str
+            :rtype: bool
+            """
+            # goal: check if a word is palindrome
+            # read from both ends, stop when two pointers meet
+            # stop when pointed char are different
+
+            # deal with special characters:
+            # 2. jump ahead when it's special char
+            # deal with two special characters back to back
+
+            s = s.lower()
+            l, r = 0, len(s) - 1
+            while l < r:
+                while l < r and not s[l].isalnum():
+                    l += 1 
+                while l < r and not s[r].isalnum():
+                    r -= 1
+                # print(s[l], s[r])
+                if s[l] != s[r]:
+                    return False
+                l += 1 
+                r -= 1
+            return True
+
 if __name__ == '__main__':
     s = Solution()
     word = "A man, a plan, a canal: Panama"
-    print(s.isPalindrome(word))
+    print(s.isPalindrome2(word))
