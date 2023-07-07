192e28078f3eb7718822d19d79f60d68a5131eb5
baggiiiie <dave23eeeeem@gmail.com>
2023-07-07 12:36:39 +0800

    Created gruopAnagrams

---
+++ arrays_hashing/groupAnagram.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        # goal: find and group anagrams
+        # return 2d array, including space and single char
+        
