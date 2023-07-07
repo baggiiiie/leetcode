e0e2e6744567e806c714afd5a514a904ce18e975
baggiiiie <dave23eeeeem@gmail.com>
2023-07-07 12:54:02 +0800

    Updated groupAnagram

--- arrays_hashing/groupAnagram.py
+++ arrays_hashing/groupAnagram.py
@@ -6,4 +6,6 @@ class Solution(object):
         """
         # goal: find and group anagrams
         # return 2d array, including space and single char
-        
+        # identify anagram: char and char counts -> dictionary
+        # unknown number of words, how many dicts to create?
+        # anagram: same char same counts -> after sort, anagrams are the same
