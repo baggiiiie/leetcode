7a60b7e6bcbbbf9acf71d7475134f886f2701dff
baggiiiie <dave23eeeeem@gmail.com>
2023-07-08 12:55:32 +0800

    Updated longestSubString

--- SlidingWindow/lengthOfLongestSubstring.py
+++ SlidingWindow/lengthOfLongestSubstring.py
@@ -4,9 +4,29 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # input: string w/ letters
+        # input: string
         # output: int
 
         # two pointers, stop when char[r] = char[l]
         # then l++, r = l + 1
         # but that means O(n^2)
+
+        ans = 0
+        l = 0
+        for l in range(len(s)):
+            traversed = []
+            r = l
+            while r < len(s):
+                if s[r] in traversed:
+                    break
+                traversed.append(s[r])
+                r += 1
+            ans = max(ans, len(traversed))
+        return ans
+
+
+if __name__ == "__main__":
+    strs = ["abcabcbb", "bbbbb", "pwwkew", " ", ""]
+    sol = Solution()
+    for s in strs:
+        print(sol.lengthOfLongestSubstring(s))
