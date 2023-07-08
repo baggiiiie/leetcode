05becb8f87cedf2ab6a7da8ebd89f6e81ffb66bd
baggiiiie <yingchao.dai22@gmail.com>
2023-07-08 09:50:40 +0800

    Updated 3sum

--- Sliding_Window/stockPrices.py
+++ Sliding_Window/stockPrices.py
@@ -19,7 +19,7 @@ class Solution(object):
         max_profit = 0
         for l, price in enumerate(prices):
             r = l + 1
-            while r < len(prices) - 1:
+            while r <= len(prices) - 1:
                 profit = prices[r] - price
                 if profit > max_profit:
                     max_profit = profit
@@ -30,7 +30,8 @@ class Solution(object):
 if __name__ == "__main__":
     nums = [
         [7,1,5,3,6,4],
-       [7,6,4,3,1]
+       [7,6,4,3,1],
+       [1,2]
     ]
     s = Solution()
     for num in nums:
