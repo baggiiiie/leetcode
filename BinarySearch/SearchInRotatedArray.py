3f50c831c8b6258ba5a009ba4ff41e001dd1f418
baggiiiie <yingchao.dai22@gmail.com>
2023-07-26 21:19:41 +0800

    added search in rotated array

--- BinarySearch/SearchInRotatedArray.py
+++ BinarySearch/SearchInRotatedArray.py
@@ -24,12 +24,12 @@ class Solution(object):
         l, r = 0, len(nums) - 1
         while l <= r:
             m = (l + r) // 2
-            print(nums[l], nums[m], nums[r])
+            # print(nums[l], nums[m], nums[r])
             if nums[l] > nums[r]:
                 if nums[m] > target:
-                    r = m - 1
-                elif nums[m] < target:
                     l = m + 1
+                elif nums[m] < target:
+                    r = m - 1
                 else:
                     return m
             else:
